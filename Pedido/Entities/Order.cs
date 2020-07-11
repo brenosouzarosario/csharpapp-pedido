6ab9e9cc5e36633941464a76f3522d8fc69e0d32
Breno Souza <brenosouza.rosario@gmail.com>
2020-07-11 18:41:15 -0300

    first commit

---
+++ Pedido/Entities/Order.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Pedido.Entities.Enums;
+
+namespace Pedido.Entities
+{
+    class Order
+    {
+        public int Id { get; set; }
+        public DateTime Moment { get; set; }
+        public OrderStatus status { get; set; }
+        public OrderStatus Status { get; internal set; }
+
+        public override string ToString()
+        {
+            return Id
+                + ", "
+                + Moment
+                + ". "
+                + status;
+        }
+    }
+}
---
+++ Pedido/Program.cs
@@ -0,0 +1,23 @@
+﻿using Pedido.Entities;
+using Pedido.Entities.Enums;
+using System;
+
+namespace Pedido
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine(order);
+
+            string txt = OrderStatus.PendingPayment.ToString();
+        }
+    }
+}
